5283e08c08df00fa42a335164f6e4a65bcc61710
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-08-21 10:30:35 -0700

    Better handling of acronyms

--- src/transformers/StringTransformer.php
+++ src/transformers/StringTransformer.php
@@ -38,10 +38,21 @@ class StringTransformer implements Transformer
 	 */
 	public function toUrl($name, $value, array $context = array()): string
 	{
+		$value = preg_replace_callback('#\b[A-Z]+\b#', [$this, 'acronymToTitle'], $value);
 		$value = str_replace('_', '-', $this->inflector->tableize($value));
 		$value = preg_replace('/[^A-Z^a-z^0-9^\/]+/', '-', $value);
-		$value = preg_replace('/-*/', '-', $value);
+		$value = preg_replace('/-+/', '-', $value);
 
 		return strtolower(trim($value, '-'));
 	}
+
+
+	/**
+	 *
+	 */
+	protected function acronymToTitle($results)
+	{
+		return ucfirst(strtolower($results[0]));
+	}
 }
+
